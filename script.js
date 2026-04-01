2d770165b943a80d2946363e1a12cabea349cf53
Anjjrm <anjali.jeremie@gmail.com>
2026-04-01 09:21:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function changeMiddleWithDesc(name, desc) {
   const middleDiv = document.getElementById('middle-name');
   middleDiv.innerText = name;
   middleDiv.setAttribute('data-desc', desc);
-
+  middleDiv.style.backgroundImage = `url('${imgUrl}')`;
   // Changer couleur selon le côté
   if (desc.toLowerCase().includes('west')) {
     middleDiv.style.color = '#00ff00'; // vert pour West Coast
